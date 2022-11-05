51913ca1e6c3807d45f001fc9f1198e30704b8b7
anmolpathry <anmolitasweet@gmail.com>
2022-11-05 11:05:47 -0600

    Changes in API_KEY

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -15,7 +15,7 @@ import GoogleMaps
   ) -> Bool {
     //print(FlutterConfigPlugin.env(for: "GOOGLE_API_KEY"))
     //GMSServices.provideAPIKey(FlutterConfigPlugin.env(for: "GOOGLE_API_KEY"))  // Add this line!
-    GMSServices.provideAPIKey("AIzaSyCA9D2VwtooUIwEWy8dzOiA-j2eGo7f488")
+    GMSServices.provideAPIKey("GOOGLE_API_KEY")
     //GMSServices.provideAPIKey(secrets.GOOGLE_API_KEY)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
