042d1612f4b12c5fb1a56d88d8f53e9a7c56db04
José Jaime Gutiérrez Martínez <42259245+JoseJaimeGtz@users.noreply.github.com>
2022-11-28 23:10:47 -0600

    Anmol (#4)
    * Create User with Email and Password
    
    * Adding products to user's pantry in Firebase
    
    * Display users products in ListView
    
    * Update pubspec.lock
    
    * Delete Info.plist
    
    * Delete GoogleService-Info.plist
    
    * Update AppDelegate.swift
    
    Co-authored-by: anmolpathry <anmolitasweet@gmail.com>

--- lib/pantry/your_food.dart
+++ lib/pantry/your_food.dart
@@ -25,12 +25,10 @@ class _YourFoodState extends State<YourFood> {
 
   @override
   Widget build(BuildContext context) {
-    final FirebaseAuth auth = FirebaseAuth.instance;
-    //final productQuery = FirebaseFirestore.instance.collection('users_pantry').doc(auth.currentUser!.uid).get();
     final productQuery = FirebaseFirestore.instance.collection('users_pantry');
+     final FirebaseAuth auth =  FirebaseAuth.instance;
     //final ingredientsQuery = productQuery.doc(auth.currentUser!.uid).get();
-
-
+    
     return BlocConsumer<AddProductsBloc, AddProductsState>(
       listener: (context, state) {},
       builder: (context, state) {
@@ -52,17 +50,23 @@ class _YourFoodState extends State<YourFood> {
                 )
               ],
             ),
-            body: FirestoreListView<Map<String, dynamic>>(
+            body: FirestoreListView<Map<String, dynamic>> (
                 query: productQuery,
-                itemBuilder: (context, snapshot) {
-                  dynamic ingredients = snapshot['ingredients'];
-                  print(ingredients);
-                  //Text("${ingredients}");
+                itemBuilder: (context, snapshot) { 
+                  bool emptyList = false;
+                  dynamic ingredients = "";
+                  snapshot.data().forEach((key, value) {
+                    if(value == auth.currentUser!.email){
+                      ingredients = snapshot["ingredients"];
+                      emptyList = true;
+                      return ingredients;
+                    } 
+                  });
                   return Column(
                     children: [
                       Padding(
                         padding: EdgeInsets.only(top: 40, bottom: 25),
-                        child: Row(
+                        child: emptyList ?  Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text("Expires soon",
@@ -73,9 +77,9 @@ class _YourFoodState extends State<YourFood> {
                             SizedBox(width: 20),
                             Icon(FontAwesomeIcons.calendarXmark)
                           ],
-                        ),
+                        ) : Text(""),
                       ),
-                      ListHeader(),
+                      emptyList ? ListHeader() : Text(""),
                       for (var i = 0; i < ingredients.length; i++)
                         ItemList(
                             product_name: ingredients[i]["product_name"],
